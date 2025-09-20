b4f2f3ed0f888591d73365045cdaa4d8f4bf4c02
connandgo <connandgo@gmail.com>
2025-09-20 21:12:29 +0900

    [feat]Assignment1

---
+++ output/k_nn.py
@@ -0,0 +1,66 @@
+from builtins import range
+from builtins import object
+import numpy as np
+
+
+class KNearestNeighbor(object):
+    """ a kNN classifier with L2 distance """
+
+    def __init__(self):
+        pass
+
+    def train(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X, k=1, num_loops=0):
+        if num_loops == 0:
+            dists = self.compute_distances_no_loops(X)
+        elif num_loops == 1:
+            dists = self.compute_distances_one_loop(X)
+        elif num_loops == 2:
+            dists = self.compute_distances_two_loops(X)
+        else:
+            raise ValueError("Invalid value %d for num_loops" % num_loops)
+
+        return self.predict_labels(dists, k=k)
+
+    # Assignment 1
+    def compute_distances_two_loops(self, X):
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        for i in range(num_test):
+            for j in range(num_train):
+                #구현part
+                dists[i][j] = np.sqrt(np.sum(np.square(X[i] - self.X_train[j])))
+        return dists
+    
+    # Assignment 2
+    def compute_distances_one_loop(self, X):        
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        for i in range(num_test):
+            # 구현 part
+            dists[i] = np.sqrt(np.sum(np.square(self.X_train - X[i]), axis = 1))
+        return dists
+
+    # Assignment 3
+    def compute_distances_no_loops(self, X):
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        # Note: (a - b)^2 = -2ab + a^2 + b^2
+        # 구현 part
+        dists = np.sqrt(np.sum(X**2, axis=1, keepdims=True) - 2 * X.dot(self.X_train.T) + np.sum(self.X_train**2, axis=1))
+        return dists
+
+    def predict_labels(self, dists, k=1):
+        num_test = dists.shape[0]
+        y_pred = np.zeros(num_test)
+        for i in range(num_test):
+            closest_y = []
+            closest_y = self.y_train[dists[i].argsort()[:k]]
+            y_pred[i] = np.argmax(np.bincount(closest_y))
+        return y_pred
